7ea05748de16c2fe5c1a167a2db40d13afd0c4e8
Yuyeji <yeji3202@naver.com>
2019-01-07 09:14:59 +0900

    radix sort

---
+++ Algo5_2.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <malloc.h>
+//#define _CRT_SECURE_NO_WARNINGS
+
+void counting_sort(int *arr, int p, int size);
+void radix_sort(int *arr, int size);
+int heap_size(int *arr);
+
+int main(void) {
+	int *arr; // 파일의 데이터를 저장할 포인터 변수 선언.
+	int size = 1;
+	FILE* f; // 파일
+	int startTime = 0, endTime = 0;
+	float gap;
+
+	f = fopen("input10000.txt", "r");
+
+	if (f == NULL) {
+		printf("파일이 열리지 않습니다.");
+		exit(1);
+	}
+
+	arr = (int *)malloc(sizeof(int));
+
+	while (feof(f) == 0) {
+		arr = (int *)realloc(arr, (size) * sizeof(int));
+		fscanf(f, "%d", &arr[size - 1]);
+		size++;
+	}
+
+	startTime = clock();
+
+	radix_sort(arr, heap_size(arr));
+
+	endTime = clock();
+	gap = (float)(endTime - startTime) / (CLOCKS_PER_SEC);
+
+
+	for (int j = 0; j < heap_size(arr); j++) {
+		printf("%d\t", arr[j]);
+	}
+
+	printf("측정 시간 : %f\n", gap);
+
+	free(arr);
+	fclose(f);
+
+	system("pause"); // 테스트 보기 위해.
+
+}
+
+int heap_size(int *arr) { //arr의 크기
+	return (_msize((void*)arr) / sizeof(int));
+}
+
+int getMax(int *arr, int size) {
+	int max = 0;
+
+	for (int i = 0; i < size; i++)
+		if (arr[i] > max)
+			max = arr[i];
+
+	return max;
+}
+
+/*couting_sort이용한 radix_sort*/
+void radix_sort(int *arr, int size) {
+	int max = getMax(arr, size); //최대값
+
+	//모든 digit에대해 counging_sort
+	for (int p = 1; max / p > 0; p *= 10) {
+		counting_sort(arr, p, size);
+	}
+}
+
+void counting_sort(int arr[], int p, int size){
+	int max = getMax(arr, size); //최대값
+	int *c, *count;
+	int i ;
+
+	c = (int *)malloc(sizeof(int)*(size));
+	count = (int *)malloc(sizeof(int)*(max+1));
+	
+	//배열 초기화
+	for (i = 0; i < 10;i++) 
+		count[i] = 0;
+	//count[]에 (arr[i] / p)%10 값 누적
+	for (i = 0; i < size; i++)
+		count[(arr[i] / p)%10]++;
+	//앞의 값을 누적
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+	//c배열에 누적하고, count[]값 감소
+	for (i = size - 1; i >= 0; i--)
+	{
+		c[count[(arr[i] / p) % 10] - 1] = arr[i];
+		count[(arr[i] /p) % 10]--;
+	}
+
+	//c배열을 arr배열에 복사
+	for (i = 0; i < size; i++)
+		arr[i] = c[i];
+}
